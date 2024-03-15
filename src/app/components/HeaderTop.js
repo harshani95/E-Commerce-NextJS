1c90c84fd37a555ef049b7c9ff094b60015c3ff9
harshani <ruwanthikapemarathna@gmail.com>
2024-03-15 18:00:27 +0530

    created top header

---
+++ src/app/components/HeaderTop.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {BsFacebook, BsTwitter, BsInstagram, BsLinkedin} from 'react-icons/bs';
+
+const HeaderTop = () => {
+    return(   
+    <div className='border-b border-gray-200 hidden sm:block' >
+        <div className='container py-4'>
+            <div className='flex justify-between items-center'>
+                <div className='hidden lg:flex gap-1'>
+                    <div className='header_top_icon_wrapper'><BsFacebook/></div>
+                    <div className='header_top_icon_wrapper'><BsTwitter /></div>
+                    <div className='header_top_icon_wrapper'><BsInstagram /></div>
+                    <div className='header_top_icon_wrapper'><BsLinkedin /></div>
+                </div>
+                <div className='text-gray-500 text-[12px]'>
+                    <strong>FREE SHIPPING</strong>THIS WEEK ORDER OVER -$50
+                </div>
+                <div className='flex gap-4'> 
+                    <select name='currency' id='currency'>
+                        <option value='USD'>USD</option>
+                        <option value='INR'>INR</option>
+                    </select>
+
+                    <select name='language' id='language'>
+                        <option value='English'>English</option>
+                        <option value='Sinhala'>Sinhala</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default HeaderTop
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container:{
+        center: true,
+        padding:"15px",
       },
+      colors: {
+        accent : "#FF8F9C",
+        blackish : "#1b1b1b",
+      }
     },
   },
   plugins: [],
