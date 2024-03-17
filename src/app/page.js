f7ed305d253586c83b99dc15911e8296e92a4ff7
harshani <ruwanthikapemarathna@gmail.com>
2024-03-17 14:18:13 +0530

    completed Header Main

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 
+import HeaderMain from "./components/HeaderMain";
 import HeaderTop from "./components/HeaderTop";
 
 export default function Home() {
   return (
     <main>
       <HeaderTop />
+      <HeaderMain />
     </main>
   );
 }
