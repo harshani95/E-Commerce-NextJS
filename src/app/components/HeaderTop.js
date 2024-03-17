6f5e1b9d78bbc9e657a1cde02c9e3948a08ac538
harshani <ruwanthikapemarathna@gmail.com>
2024-03-17 14:02:55 +0530

    completed header top

--- src/app/components/HeaderTop.js
+++ src/app/components/HeaderTop.js
@@ -13,15 +13,16 @@ const HeaderTop = () => {
                     <div className='header_top_icon_wrapper'><BsLinkedin /></div>
                 </div>
                 <div className='text-gray-500 text-[12px]'>
-                    <strong>FREE SHIPPING</strong>THIS WEEK ORDER OVER -$50
+                    <strong>FREE SHIPPING</strong> THIS WEEK ORDER OVER - $50
                 </div>
                 <div className='flex gap-4'> 
-                    <select name='currency' id='currency'>
-                        <option value='USD'>USD</option>
+                    <select name='currency' id='currency' className="text-gray-500 text-[12px] w-[70px]">
+                        <option value='USD $'>USD</option>
+                        <option value="EUR €">EUR €</option>
                         <option value='INR'>INR</option>
                     </select>
 
-                    <select name='language' id='language'>
+                    <select name='language' id='language'  className="text-gray-500 text-[12px] w-[80px]">
                         <option value='English'>English</option>
                         <option value='Sinhala'>Sinhala</option>
                     </select>
