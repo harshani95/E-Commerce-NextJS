f89b63919665dc5236892d334e4fcde877b48597
harshani <ruwanthikapemarathna@gmail.com>
2024-03-18 18:24:05 +0530

    Completed Footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4">
+    Copyright © Stack <br /> All Rights Reserved 2024.
+    </div>
+  )
+}
+
+export default Footer
